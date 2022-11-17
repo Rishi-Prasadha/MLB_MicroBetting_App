8804898adb2c557e219041a44229b0e70d79bd61
Brittanie <brittanie.polasek@yahoo.com>
2022-11-16 22:28:51 -0600

    im tired

--- brittanie/testing/ethereum.py
+++ brittanie/testing/ethereum.py
@@ -9,32 +9,15 @@ from web3 import middleware
 from web3.gas_strategies.time_based import medium_gas_price_strategy
 from web3 import Web3
 
-w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
-w3_2 = Web3(Web3.HTTPProvider('http://127.0.0.1:7545'))
+# w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
+# w3_2 = Web3(Web3.HTTPProvider('http://127.0.0.1:7545'))
 
 # Create a function called `generate_account` that automates the Ethereum
 # account creation process
-def generate_account8545(w3):
+def generate_account_sender(w3):
     """Create a digital wallet and Ethereum account from a mnemonic seed phrase."""
     # Access the mnemonic phrase from the `.env` file
-    mnemonic = os.getenv("MNEMONIC8545")
-
-    # Create Wallet object instance
-    wallet = Wallet(mnemonic)
-
-    # Derive Ethereum private key
-    private, public = wallet.derive_account("eth")
-
-    # Convert private key into an Ethereum account
-    account = Account.privateKeyToAccount(private)
-
-    # Return the account from the function
-    return account
-
-def generate_account7545(w3):
-    """Create a digital wallet and Ethereum account from a mnemonic seed phrase."""
-    # Access the mnemonic phrase from the `.env` file
-    mnemonic = os.getenv("MNEMONIC7545")
+    mnemonic = os.getenv("MNEMONIC")
 
     # Create Wallet object instance
     wallet = Wallet(mnemonic)
@@ -61,7 +44,7 @@ def get_balance(w3, address):
     return ether
 
 # Create a function called `send_transaction` that creates a raw transaction, signs it, and sends it. Return the confirmation hash from the transaction
-def send_transaction(w3, account, receiver, ether):
+def send_transaction(w3, sender, receiver, ether):
     """Send an authorized transaction."""
     # Set a medium gas price strategy
     w3.eth.setGasPriceStrategy(medium_gas_price_strategy)
@@ -70,20 +53,20 @@ def send_transaction(w3, account, receiver, ether):
     wei_value = w3.toWei(ether, "ether")
 
     # Calculate gas estimate
-    gas_estimate = w3.eth.estimateGas({"to": receiver.address, "from": account.address, "value": wei_value})
+    gas_estimate = w3.eth.estimateGas({"to": receiver, "from": sender.address, "value": wei_value})
 
     # Construct a raw transaction
     raw_tx = {
-        "to": receiver.address,
-        "from": account.address,
+        "to": receiver,
+        "from": sender.address,
         "value": wei_value,
         "gas": gas_estimate,
         "gasPrice": 0,
-        "nonce": w3.eth.getTransactionCount(account.address)
+        "nonce": w3.eth.getTransactionCount(sender.address)
     }
 
     # Sign the raw transaction with ethereum account
-    signed_tx = account.signTransaction(raw_tx)
+    signed_tx = sender.signTransaction(raw_tx)
 
     # Send the signed transactions
     return w3.eth.sendRawTransaction(signed_tx.rawTransaction)
